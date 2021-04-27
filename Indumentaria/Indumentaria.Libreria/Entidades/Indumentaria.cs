58918361dea9915d25ad47d196a316463bd62a66
Renard <rdlafitte@gmail.com>
2021-04-27 11:51:54 -0300

    Inicio indumentaria

---
+++ Indumentaria/Indumentaria.Libreria/Entidades/Indumentaria.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indumentaria.Libreria.Entidades
+{
+    abstract class Indumentaria
+    {
+        private TipoIndumentaria _tipo;
+        private int _codigo;
+        private int _stock;
+        private string _talle;
+        private double _precio;
+
+        public int Codigo { get => _codigo; set => _codigo = value; }
+        public int Stock { get => _stock; set => _stock = value; }
+        public string Talle { get => _talle; set => _talle = value; }
+        public double Precio { get => _precio; set => _precio = value; }
+        internal TipoIndumentaria Tipo { get => _tipo; set => _tipo = value; }
+
+        public string ToString()
+        {
+            return $"Código: {this.Codigo} - Stock: {this.Stock} unidades - Talle: {this.Talle} - Precio: ${this.Precio}";
+        }
+        protected virtual bool Equals(Indumentaria i)
+        {
+            Indumentaria aux = (Indumentaria)i;
+            if (aux.Codigo == i.Codigo)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string GetDetalle()
+        {
+
+        }
+    }
+}
---
+++ Indumentaria/Indumentaria.Libreria/Entidades/TipoIndumentaria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indumentaria.Libreria.Entidades
+{
+    abstract class TipoIndumentaria
+    {
+        private string _origen;
+        private double _porcentajeAlgodon;
+
+        public string Origen { get => _origen; set => _origen = value; }
+        public double PorcentajeAlgodon { get => _porcentajeAlgodon; set => _porcentajeAlgodon = value; }
+    }
+}
